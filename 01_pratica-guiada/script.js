c5ba958ca5f6c08e30e1f3913dc8b79801f75b48
Rafael Machado <machadorafael27@gmail.com>
2023-05-04 21:11:39 -0300

    operadores-template2

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,11 @@
 //CODIGO AQUI
+console.log("----------------------Boa Noite-----------------------");
+let idade = Number(prompt("Poderia informar sua idade?"));
+let idadeAmig = Number(prompt("Poderia informar a idade da sua amiga?"));
+console.log("Sua idade é maior que a do seu melhor amigo?" + (idade > idadeAmig));
+let resultado = Number(Math.abs(idade - idadeAmig));
+console.log(`A diferença de idade é ${resultado}`);
+
+
+
+ 
